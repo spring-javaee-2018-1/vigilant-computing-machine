5b607d702dca58db98852446e8f198eb806838b5
Marco Mangan <marco.mangan@pucrs.br>
2018-05-25 15:31:02 -0300

    #2

--- hello/src/main/java/exemple/hello/App.java
+++ hello/src/main/java/exemple/hello/App.java
@@ -8,6 +8,13 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( getGreeting() );
     }
+
+	/**
+	 * @return
+	 */
+	public static String getGreeting() {
+		return "Hello, World!";
+	}
 }
